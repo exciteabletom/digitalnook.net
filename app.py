8dbab45a789c8df99045adcb6c4fa8d0fcdc12da
Tom Dougiamas <tom@dougiamas.com>
2019-09-16 15:10:42 +0000

    incremental fixes

--- app.py
+++ app.py
@@ -1,56 +1,54 @@
 from flask import Flask, render_template
-application = Flask(__name__)
+app = Flask(__name__)
 
 
 # pie
-@application.route('/pie', host='digitalnook.net')
+@app.route('/pie', host='digitalnook.net')
 def pie():
     return render_template('pie.html')
 
 # Default page
-@application.route('/',host='digitalnook.net')
+@app.route('/',host='digitalnook.net')
 def index():
     return render_template('index.html')
-
 # bruh
-@application.route('/bruh', host='digitalnook.net')
+@app.route('/bruh', host='digitalnook.net')
 def bruh():
     return render_template('bruh.html')
 
 # games index
-@application.route('/games', host='digitalnook.net')
+@app.route('/games', host='digitalnook.net')
 def games():
     return render_template('games.html')
 
 # Rock paper scissors
-@application.route('/games/rps', host='digitalnook.net')
+@app.route('/games/rps', host='digitalnook.net')
 def rps():
     return render_template('rps.html')
 
 # Bomb Defusal Game
-@application.route('/games/bomb', host='digitalnook.net')
+@app.route('/games/bomb', host='digitalnook.net')
 def bomb():
     return render_template('bomb.html')
 
 # reaction speed game
-@application.route('/games/reaction', host='digitalnook.net')
+@app.route('/games/reaction', host='digitalnook.net')
 def reaction():
     return render_template('reaction.html')
 
 # Handles 404 errors
-@application.errorhandler(404)
-
+@app.errorhandler(404)
 def not_found(e):
     return render_template("404.html")
 
 # test for subdomains
-#@application.route("/", subdomain="tom", host="digitalnook.net")
+#@app.route("/", subdomain="tom", host="digitalnook.net")
 
 #def tom():
 #    return render_template("test.html")
 
 if __name__ == '__main__':
 #    website_url = "digitalnook.net"
-#    @application.config['SERVER NAME'] = website_url
-    application.run(host="10.1.1.50", port='80')
+#    @app.config['SERVER NAME'] = website_url
+    app.run(host="10.1.1.50", port='80')
 
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
-from app import application
+from app import app
 
 if __name__ == "__main__":
-    application.run()
+    app.run()
 
 
