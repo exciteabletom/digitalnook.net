fdd17190ced8f0e914eb31dae3a30cbcc19b01dc
exciteabletom <tom@digitalnook.net>
2019-10-30 22:57:08 +0800

    bruh

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, request, redirect 
 import os
 import json
 from nocache import nocache
