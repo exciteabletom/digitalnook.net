442b3e65e94fe723cb5c60cc28a9db320ef9c8c5
exciteabletom <admin@digitalnook.net>
2020-07-30 22:00:49 +0800

    Minor edits

--- scratch.py
+++
@@ -1,8 +0,0 @@
-finalData = []
-with open("./nouns.txt", "r") as nouns:
-	raw = nouns.read()
-
-	finalData = raw.split("\n")
-
-with open("./formattedNouns.txt", "w") as formatted:
-	formatted.write(str(finalData))
