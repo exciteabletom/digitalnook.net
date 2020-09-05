f5ae7bb9b05d6789fec47e5e9fc460bdaac50af5
exciteabletom <tom@digitalnook.net>
2020-09-06 00:23:19 +0800

    Made a OS agnostic python install script

---
+++ install.py
@@ -0,0 +1,163 @@
+########################################################################
+# install.py - Copyright 2020, Thomas Chris Dougiamas
+#
+# This file is part of Digital Nook.
+#
+# Digital Nook is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Digital Nook is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Digital Nook.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################
+
+import os
+import sys
+import platform
+import venv
+import subprocess
+from pathlib import Path
+
+# OS agnostic file seperator
+fs_sep = str(Path("/"))
+
+def status(message=None):
+	"""
+	Void function
+	Prints a formatted status message. 
+	If no status message prints 'done'
+	"""
+	
+	if not message:
+		message = "Done\n"
+
+	print(f">>> {message}")
+
+
+
+def check(question: str):
+	"""
+	Ask user a Yes/No question.
+
+	:param question: The question the user will be asked
+	:r_type: bool
+	:return: True if user said yes, false otherwise
+	"""
+	
+	yes_or_no = input(f"{question} (y/n): ").lower()
+
+	if yes_or_no == "y":
+		print("\n", end="")
+		return True
+	else:
+		return False
+
+
+def create_venv():
+	"""
+	Void Function
+	Creates a new virtual environment with name .venv in the working directory.
+
+	:r_type: bool
+	:return: True on success, false otherwise
+	"""
+	try:
+		# This might fail on FAT filesystems or windows
+		venv.create(".venv", symlinks=True)
+
+	except:
+		# Fallback command
+		try:
+			venv.create(".venv")
+		except:
+			return False
+	
+	return True
+
+
+
+def main():
+	"""
+	Main entrypoint. Installs a new Digital Nook dev environment.
+	"""
+
+	# If the operating system is unix based or not
+	unix_os = False
+
+	# TODO: More operating systems covered here
+	if platform.system() in ("Linux", "Darwin"):
+		unix_os = True
+
+	# Where the python binary is
+	python_bin = "python3"
+
+	script_path = sys.argv[0]
+	script_dir = ""
+
+	if fs_sep in script_path:
+		# Get the directory the script is in
+		script_dir_lst = script_path.split(fs_sep)[:-1]
+		script_dir = fs_sep.join(script_dir_lst)
+
+		# Change to that directory
+		os.chdir(script_dir)
+		print(os.listdir())
+
+
+	# It is now safe to assume that the working directory
+	# is the project's root.
+
+	# Quick are you sure check
+	if not check("This script will delete any existing databases.\nAre you sure?"):
+		sys.exit(0)
+	
+	if check("Do you want to create a virtual environment?\nThis is highly recommended."):
+		status("Creating virtual environment")
+		if not create_venv():
+			print("Error creating a virtual environment")
+			sys.exit(1)
+
+		python_bin = ".venv/bin/python3"
+
+		# Update pip and setuptools
+		subprocess.call([python_bin, "-m", "pip", "install", "--upgrade", "pip", "setuptools"])
+
+		status()
+	
+	status("Installing dependencies")
+	subprocess.call([python_bin, "-m", "pip", "install", "-r", "install/requirements.txt"])
+	status()
+
+	status("Creating new database")
+	os.remove("userdata.db")
+	subprocess.call([python_bin, "install/create_tables.py"])
+	status()
+
+	status("Creating latestId flatfile")
+	with open("static/latestId", "w") as f:
+		f.write("0")
+	status()
+
+	status("Creating login key")
+	subprocess.call([python_bin, "generateKey.py", "-f"])
+	status()
+
+	if unix_os:
+		source_instruct = ". .venv/bin/activate"
+	else:
+		source_instruct = ".venv\\Scripts\\activate.bat"
+
+	print(
+f"""All tasks done!
+
+If you made a virtual environment run it with '{source_instruct}'
+You can run the server with 'python3 -m flask run'""")	
+
+if __name__ == "__main__":
+	main()
---
+++ install/create_tables.py
@@ -0,0 +1,32 @@
+########################################################################
+# create_tables.py - Copyright 2020, Thomas Chris Dougiamas
+#
+# This file is part of Digital Nook.
+#
+# Digital Nook is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Digital Nook is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Digital Nook.  If not, see <https://www.gnu.org/licenses/>.
+########################################################################
+
+import sqlite3
+
+con = sqlite3.connect("userdata.db")
+cur = con.cursor()
+
+with open("install/create_tables.sql", "r") as sql:
+	for query in sql.read().split("\n"):
+		cur.execute(query)
+	con.commit()
+	con.close()
+	
+	
+
