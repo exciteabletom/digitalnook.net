4238c424c904952ba48cd9a2ec63727616ae10b6
Tommy Dougiamas <tom@digitalnook.net>
2021-02-12 21:12:00 +0800

    Update create_tables.sql

--- install/create_tables.sql
+++ install/create_tables.sql
@@ -1,4 +1,4 @@
--- Queries for initialising an empty DB separated by newlines
+-- Queries for initialising an empty DB, separated by newlines
 CREATE TABLE main (id INT PRIMARY KEY NOT NULL, name TEXT NOT NULL, password BLOB NOT NULL, postID STRING);
 CREATE TABLE drawSomething (gameId BLOB NOT NULL, word TEXT NOT NULL, image BLOB, guesses INT, gameResult INT);
 CREATE TABLE drawSomethingStats (name TEXT NOT NULL, drawingsMade INT, drawingsGuessed INT);
